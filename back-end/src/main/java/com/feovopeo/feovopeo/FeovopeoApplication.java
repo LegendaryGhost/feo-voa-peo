df051333cfe62752685c2a6d2087d5762bdc7e2b
sergianafr <sergiravelo@gmail.com>
2025-07-26 12:18:50 +0300

    init

---
+++ back-end/src/main/java/com/feovopeo/feovopeo/FeovopeoApplication.java
@@ -0,0 +1,13 @@
+package com.feovopeo.feovopeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeovopeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeovopeoApplication.class, args);
+	}
+
+}
