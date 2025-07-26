dec2b08b9946913571ba71dd43b3d564501dd1de
= <=>
2025-07-26 12:25:47 +0300

    Intial commit

---
+++ back-end/src/main/java/com/feovaopeo/feovoapeo/FeovoapeoApplication.java
@@ -0,0 +1,13 @@
+package com.feovaopeo.feovoapeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeovoapeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeovoapeoApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/feovaopeo/feovoapeo/FeovoapeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feovaopeo.feovoapeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeovoapeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
